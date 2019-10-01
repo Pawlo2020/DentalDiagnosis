7c37d815251c4d8c478002da3b000c88d183e268
Paweł Szeląg <pawlodev@outlook.com>
2019-10-01 13:58:04 +0200

    Initial commit

---
+++ src/Program.java
@@ -0,0 +1,18 @@
+public class Program {
+
+    static MainWindow window = new MainWindow();
+    public static void main(String [] args){
+
+        window.setVisible(true);
+
+        window.setSize(960,720);
+
+        
+
+
+    }
+
+
+
+
+}
